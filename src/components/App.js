19fd9616ed712cbfdd8ccbc88f912c839fe0342c
Stormi Anduaga-Arias <stormarias@gmail.com>
2023-06-01 03:43:17 -0700

    Add an About and Header section.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import blogData from "../data/blog";
-
-console.log(blogData);
+import Header from "./Header";
+import About from "./About";
 
 function App() {
+  const {name, image, about} = blogData;
+
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header name={name} />
+      <About image={image} about={about}/>
     </div>
   );
 }
